a1d954be43feb1d364d1d70f23465f18eec3685f
herm4nn <1990@holbertonschool.com>
2020-06-13 02:29:38 +0000

    6/12/2020

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  char a;
-  for (a = 'a'; a <= 'z'; a++)
-    if (a != 'e' && a != 'q')
-      putchar(a);
-  putchar('\n');
-  return (0);
+char a;
+for (a = 'a'; a <= 'z'; a++)
+if (a != 'e' && a != 'q')
+putchar(a);
+putchar('\n');
+return (0);
 }
