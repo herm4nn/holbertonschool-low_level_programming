f76b893b4fce5b11f93722a3eddda26ac684a036
herm4nn <1990@holbertonschool.com>
2020-06-19 04:38:56 +0000

    6/18/2020

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * print_alphabet - prints the alphabet
-  *
-   * Return: Always 0.
-    */
+* print_alphabet - prints the alphabet
+*
+* Return: Always 0.
+*/
 void print_alphabet(void)
 {
 int a;
