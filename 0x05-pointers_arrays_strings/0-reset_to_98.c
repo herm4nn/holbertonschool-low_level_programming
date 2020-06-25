b884dddc7b5e8464834b2d849c96e0ec6fb0525d
herm4nn <1990@holbertonschool.com>
2020-06-25 02:15:21 +0000

    6/25/2020

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main (void reset_to_98(int *ptr));
+
+{
+  int *ptr;
+
+ 
