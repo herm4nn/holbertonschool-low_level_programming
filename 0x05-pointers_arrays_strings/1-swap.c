d0126fae9ce057d2633a1a7126fb60f5467c8a51
herm4nn <1990@holbertonschool.com>
2020-06-25 14:58:12 +0000

    6/30/2020

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,3 +6,24 @@
  *
  * Return: Always 0.
  */
+void swap(int*, int*);
+
+int main(void)
+{
+int x, y;
+printf("Enter the value of x and y\n");
+scanf("%d%d", &x, &y);
+printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+swap(&x, &y);
+printf("After Swapping\nx = %d\ny = %d\n", x, y);
+return (0);
+}
+/**Swap function definition*/
+void swap(int *a, int *b)
+{
+int t;
+
+t  = *b;
+*b = *a;
+*a = t;
+}
