1ddfb8399781d108adf6d21d132711037ce7287e
herm4nn <1990@holbertonschool.com>
2020-06-25 02:30:15 +0000

    6/25/2020

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,3 +8,15 @@
  */
 void swap_int(int *a, int *b);
 {
+    int a, b;
+   
+    printf("Input two integers (a & b) to swap\n");
+    scanf("%d%d", &a, &b);
+   
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("a = %d\nb = %d\n",a,b);
+    return 0;
+  }
